5c0947dd477a6580f4e8bbfa4a877451dd5b072c
SoonYang <k17263636@gmail.com>
2017-06-07 11:35:42 +0800

    建立方案、專案、測試專案

---
+++ PotterCart/PotterCart/PotterCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterCart
+{
+    public class PotterCart
+    {
+        public decimal CalculateFee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotterCart/PotterCartTests/PotterCartTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterCart.Tests
+{
+    [TestClass()]
+    public class PotterCartTests
+    {
+    }
+}
