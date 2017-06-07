c3c7319ed7c6a1cd9456bb3b29e605b7241e9013
SoonYang <k17263636@gmail.com>
2017-06-07 12:15:30 +0800

    新增 class : Book、BookHarryPotter

---
+++ PotterCart/PotterCart/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterCart
+{
+    /// <summary>
+    /// 書
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// 名稱
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 價格
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
---
+++ PotterCart/PotterCart/BookHarryPotter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterCart
+{
+    /// <summary>
+    /// 哈利波特系列
+    /// </summary>
+    public class BookHarryPotter : Book
+    {
+    }
+}
